b83541195e3e00f5280b6dba6280112fbaf303f3
Mark-Ameka <xenonmendozaxenon@gmail.com>
2024-06-10 17:10:27 +0800

    stepper.form added

--- resources/views/livewire/pages/stepper/initial.blade.php
+++ resources/views/livewire/pages/stepper/initial.blade.php
@@ -5,18 +5,69 @@ use Livewire\Attributes\Layout;
 use Livewire\Attributes\Validate;
 use App\Models\User;
 use App\Models\Tutee;
+use App\Models\Institute;
 use App\Models\Tutor;
 
 new #[Layout('layouts.app')] class extends Component {
     public $count = 1;
+
+    // General
     public $user_type = '';
+    public $dates = [''];
+    public $inputs = [];
+
+    // Tutee
     public $grade_level = '';
+    public $from = [];
+    public $to = [];
+    public $institute = [];
+
+    // Tutor
     public $test = '';
+
+    public function mount()
+    {
+        $this->dates = range(1990, 2024);
+        $this->inputs = [0];
+        $this->from = [''];
+        $this->to = [''];
+        $this->institute = [''];
+    }
+
+    public function add_institute()
+    {
+        if (count($this->inputs) <= 3) {
+            $this->inputs[] = count($this->inputs);
+            $this->from[] = ''; // Ensure from, to, and institute arrays are synchronized
+            $this->to[] = '';
+            $this->institute[] = '';
+        }
+    }
+
+    public function remove_institute($index)
+    {
+        unset($this->inputs[$index]);
+        unset($this->from[$index]);
+        unset($this->to[$index]);
+        unset($this->institute[$index]);
+
+        $this->inputs = array_values($this->inputs);
+        $this->from = array_values($this->from);
+        $this->to = array_values($this->to);
+        $this->institute = array_values($this->institute);
+    }
+
+
 
     public function next_step()
     {
         $this->validate_status();
         $this->count++;
+    }
+
+    public function prev_step()
+    {
+        $this->count--;
     }
 
     public function validate_status()
@@ -30,9 +81,17 @@ new #[Layout('layouts.app')] class extends Component {
         switch ($this->user_type) {
             case $this->user_type === 'tutee':
                 if ($this->count === 2) {
-                    $this->validate([
-                        'grade_level' => 'required',
-                    ]);
+                    $this->validate(
+                        [
+                            'from.*' => 'required|date',
+                            'to.*' => 'required|date|after:from.*',
+                            'institute.*' => 'required|max:200',
+                            'grade_level' => 'required',
+                        ],
+                        [
+                            'to.*.after' => 'The "to" date must be after the "from" date.',
+                        ],
+                    );
                 } elseif ($this->count === 3) {
                     # code...
                 }
@@ -52,16 +111,26 @@ new #[Layout('layouts.app')] class extends Component {
 
     public function submit()
     {
+        // Update stepper to 0
         $user = User::find(Auth::id());
         $user->user_type = $this->user_type;
         $user->is_stepper = 0;
         $user->save();
 
         if ($user && $this->user_type === 'tutee') {
-            Tutee::create([
+            $tutee = Tutee::create([
                 'user_id' => $user->id,
                 'grade_level' => $this->grade_level,
             ]);
+
+            foreach ($this->inputs as $item) {
+                Institute::create([
+                    'tutee_id' => $tutee->id,
+                    'from' => $this->from[$item],
+                    'to' => $this->to[$item],
+                    'institute' => $this->institute[$item],
+                ]);
+            }
 
             return redirect()->route('dashboard');
         } elseif ($user && $this->user_type === 'tutor') {
@@ -109,6 +178,8 @@ new #[Layout('layouts.app')] class extends Component {
 
     @switch($count)
         @case($count < 4)
+            <button wire:click='prev_step' @if ($count === 1) @disabled(true) @endif>Back</button>
+
             <button wire:click='next_step'>Next</button>
         @break
 
--- resources/views/livewire/pages/stepper/tutee/form.blade.php
+++ resources/views/livewire/pages/stepper/tutee/form.blade.php
@@ -4,3 +4,29 @@
         <x-wui-select.option label="College" value="college" />
     </x-wui-select>
 </div>
+
+<div>
+    @foreach ($inputs as $index => $input)
+        <div>
+            <x-wui-select label="From" placeholder="1990" wire:model.defer="from.{{ $index }}" id="from.{{ $index }}" name="from.{{ $index }}">
+                @foreach ($dates as $year)
+                    <x-wui-select.option label="{{ $year }}" value="{{ $year }}-01-01" />
+                @endforeach
+            </x-wui-select>
+            <x-wui-select label="To" placeholder="2000" wire:model.defer="to.{{ $index }}" id="to.{{ $index }}" name="to.{{ $index }}">
+                @foreach ($dates as $year)
+                    <x-wui-select.option label="{{ $year }}" value="{{ $year }}-01-01" />
+                @endforeach
+            </x-wui-select>
+        </div>
+
+        <div>
+            <x-wui-input label="Institute" id="institute.{{ $index }}" name="institute.{{ $index }}"
+                placeholder="Institute" wire:model='institute.{{ $index }}' />
+
+        </div>
+        <x-wui-button wire:click='remove_institute({{ $index }})' emerald label="Remove" />
+    @endforeach
+    <x-wui-button wire:click='add_institute' wire:loading.attr='@disabled(true)'
+        wire:target='add_institute' emerald label="Add Insitute" />
+</div>
