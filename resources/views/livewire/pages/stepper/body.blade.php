21f7874660e071b132c244ec052ef446c2830e8a
Mark-Ameka <xenonmendozaxenon@gmail.com>
2024-06-20 18:13:13 +0800

    Revert "This commit is a mistake"
    This reverts commit 580cd50dc3a50ef2e64ab1f134460c611c191bc3.

--- resources/views/livewire/pages/stepper/body.blade.php
+++ resources/views/livewire/pages/stepper/body.blade.php
@@ -4,30 +4,42 @@
             @include('livewire.pages.stepper.header')
         </div>
         <div class="md:col-span-5 row-span-4">
-            @if ($user_type === 'tutee')
-                @includeWhen($count === 2, 'livewire.pages.stepper.tutee.steps.form')
-                @includeWhen($count === 3, 'livewire.pages.stepper.tutee.steps.fields')
-            @elseif ($user_type === 'tutor')
-                @includeWhen($count === 2, 'livewire.pages.stepper.tutor.steps.form')
-                @includeWhen($count === 3, 'livewire.pages.stepper.tutor.steps.fields')
-            @endif
+            @if ($count === 1)
+                @include('livewire.pages.stepper.role')
 
-            @includeWhen($count === 4, 'livewire.pages.stepper.confirm')
-
-            <div class="flex justify-between w-3/4 mx-auto gap-3">
-                <x-secondary-button wire:click.prevent='prev_step' class="w-full @if ($count === 1)
-                    hidden
-                @endif">
-                    Back
-                </x-secondary-button>
-                @if ($count < 4)
+                <div class="flex justify-between w-2/3 mx-auto gap-3">
                     <x-primary-button wire:click.prevent='next_step' class="w-full">
                         Next
                     </x-primary-button>
+                </div>
+            @else
+                @if (session('error'))
+                    <div class="text-red-500">
+                        {{ session('error') }}
+                    </div>
                 @endif
-            </div>
+
+                @if ($user_type === 'tutee')
+                    @includeWhen($count === 2, 'livewire.pages.stepper.tutee.steps.form')
+                    @includeWhen($count === 3, 'livewire.pages.stepper.tutee.steps.fields')
+                @elseif ($user_type === 'tutor')
+                    @includeWhen($count === 2, 'livewire.pages.stepper.tutor.steps.form')
+                    @includeWhen($count === 3, 'livewire.pages.stepper.tutor.steps.fields')
+                @endif
+
+                @includeWhen($count === 4, 'livewire.pages.stepper.confirm')
+
+                <div class="flex justify-between w-3/4 mx-auto gap-3">
+                    <x-secondary-button wire:click.prevent='prev_step' class="w-full">
+                        Back
+                    </x-secondary-button>
+                    @if ($count < 4)
+                        <x-primary-button wire:click.prevent='next_step' class="w-full">
+                            Next
+                        </x-primary-button>
+                    @endif
+                </div>
+            @endif
         </div>
     </div>
 </div>
-
-
