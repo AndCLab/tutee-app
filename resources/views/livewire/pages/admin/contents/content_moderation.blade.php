2c593e341b7b62756f2587b285335b09ba8ac8c0
mariahariannie <mariahariannie@gmail.com>
2024-10-27 13:35:21 +0800

    admin: content moderation - view content modal

--- resources/views/livewire/pages/admin/contents/content_moderation.blade.php
+++ resources/views/livewire/pages/admin/contents/content_moderation.blade.php
@@ -19,6 +19,9 @@ new #[Layout('layouts.admin')] class extends Component {
     public $report_options = [];
     public $availableOptions = [];
 
+    public $viewContentModal = false;
+    public $getContent;
+
     public function mount()
     {
         $this->availableOptions = ReportContent::distinct()
@@ -29,7 +32,7 @@ new #[Layout('layouts.admin')] class extends Component {
     public function updatedSelectAll($value)
     {
         if ($value) {
-            $this->selected = $this->getDataQuery()->pluck('reporter_id')->toArray();
+            $this->selected = $this->getDataQuery()->pluck('id')->toArray();
         } else {
             $this->selected = [];
         }
@@ -83,6 +86,12 @@ new #[Layout('layouts.admin')] class extends Component {
             })
             ->paginate($this->perPage);
     }
+
+    public function openContentModal($id)
+    {
+        $this->viewContentModal = true;
+        $this->getContent = ReportContent::findOrFail($id);
+    }
 }; ?>
 
 <section>
@@ -120,7 +129,6 @@ new #[Layout('layouts.admin')] class extends Component {
                                 <x-wui-button label="Select Content" flat green sm icon='clipboard-check'/>
                             </x-slot>
 
-                            <x-wui-dropdown.item label="View Content" wire:click="openContentModal"/>
                             <x-wui-dropdown.item label="Approved" wire:click="reportContentStatus('Approved')"/>
                             <x-wui-dropdown.item label="Not Approved" wire:click="reportContentStatus('Not Approved')"/>
                         </x-wui-dropdown>
@@ -166,6 +174,11 @@ new #[Layout('layouts.admin')] class extends Component {
                                 'name' => 'status',
                                 'displayName' => 'Status'
                             ])
+
+                            @include('livewire.pages.tutor.schedule.includes.sort-icons-table', [
+                                'name' => 'action',
+                                'displayName' => 'Action'
+                            ])
                         </tr>
                     </thead>
 
@@ -199,6 +212,11 @@ new #[Layout('layouts.admin')] class extends Component {
                                 <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
                                     {{ $report->status }}
                                 </td>
+
+                                <td class="whitespace-nowrap px-4 py-1 text-gray-700">
+                                    <x-wui-button slate xs class="w-full" label="View Content"
+                                    wire:click="openContentModal({{ $report->id }})"/>
+                                </td>
                             </tr>
                         @empty
                             <tr>
@@ -214,5 +232,98 @@ new #[Layout('layouts.admin')] class extends Component {
             </div>
         </div>
     </div>
+
+    @if ($getContent)
+    {{-- View Content Modal --}}
+    <x-wui-modal.card wire:model="viewContentModal" class="space-y-3" align='center' max-width='xl'>
+        <div class="flex gap-2 items-start">
+            <div class="size-16">
+                <img
+                    alt="Reporter Avatar"
+                    src="{{ $getContent->reporter->avatar ? Storage::url($getContent->reporter->avatar) : asset('images/default.jpg') }}"
+                    class="rounded-full object-cover border border-[#F1F5F9] overflow-hidden"
+                />
+            </div>
+            <div class="w-full space-y-2">
+                <p class="flex gap-2 font-semibold">
+                    {{ $getContent->reporter->fname .' '. $getContent->reporter->lname}}
+                </p>
+            </div>
+        </div>
+
+        {{-- class name --}}
+        <div class="flex flex-col font-semibold">
+            Reason
+            <span class="font-light">
+                {{ $getContent->report_option}}
+            </span>
+        </div>
+
+        {{-- class description --}}
+        <div class="flex flex-col font-semibold">
+            <div class="flex gap-2 items-center">
+                Feedback
+            </div>
+            <span class="font-light">
+                {{ $getContent->comment}}
+            </span>
+        </div>
+
+        {{-- collapsable class details --}}
+        <div class="p-3 py-2 rounded-md bg-[#E1E7EC]" x-data="{ expanded: false }">
+            <div class="flex cursor-pointer justify-between items-center" @click="expanded = ! expanded">
+                <span class="font-semibold text-sm">Reported Content Details</span>
+                <template x-if='expanded == false' x-transition>
+                    <x-wui-button xs label='View more' icon='arrow-down'
+                        flat />
+                </template>
+                <template x-if='expanded == true' x-transition>
+                    <x-wui-button xs label='View less' icon='arrow-up'
+                        flat />
+                </template>
+            </div>
+            <div class="text-sm" x-show="expanded" x-collapse x-cloak>
+                @if($getContent->class_id)
+                    <p>
+                        <strong>Class ID: </strong> {{ $getContent->class_id }}
+                    </p>
+                    <p>
+                        <strong>Class Name: </strong> {{ $getContent->class->class_name }}
+                    </p>
+                    <p>
+                        <strong>Class by: </strong> {{ $getContent->class->tutor->user->fname .' '. $getContent->class->tutor->user->lname }}
+                    </p>
+                @elseif ($getContent->post_id)
+                    <p>
+                        <strong>Post ID: </strong> {{ $getContent->post_id }}
+                    </p>
+                    <p>
+                        <strong>Post Description: </strong> {{ $getContent->post->post_desc }}
+                    </p>
+                    <p>
+                        <strong>Posted by: </strong> {{ $getContent->post->tutees->user->fname .' '. $getContent->post->tutees->user->lname}}
+                    </p>
+                @endif
+
+                <p>
+                    <strong>Report Status: </strong> {{ $getContent->status }}
+                </p>
+            </div>
+        </div>
+
+        <x-slot name='footer'>
+            <div class="flex items-center justify-between font-light text-xs pt-2">
+                <div class="flex gap-2 items-center text-[#64748B]">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                    </svg>
+                    <p>Reported on {{ $getContent->date_reported->format('l, F d Y g:i A') }}</p>
+                </div>
+            </div>
+        </x-slot>
+    </x-wui-modal.card>
+    @endif
 </section>
 
